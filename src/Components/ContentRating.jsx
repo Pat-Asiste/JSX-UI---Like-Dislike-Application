dc286ea740a6d36e64436828f3fdc5420cfd9ede
Pat-Asiste <barrantespatrick@gmail.com>
2025-11-12 18:43:24 -0500

    Edited ContentRating.JSX --- added render()

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -3,16 +3,28 @@ import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            like: 0,
+            dislike: 0
+        }
+    }
+    render() {
+        return (
+            <>
+                <div className='content-rating'>
+                    <p>
+                        “Los buenos tiempos se convierten en buenos recuerdos, pero los malos tiempos se convierten en buenas lecciones.”
+                    </p>
+                </div>
+                <div className='rating-buttons'>
+                    <button>Like ({this.state.likes})</button>
+                    <button>Dislike ({this.state.dislike})</button>
+                </div>
+            </>
+        );
+    }
 }
 
 export default ContentRating;
